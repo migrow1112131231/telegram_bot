780058a362b20f066c65ab1e691a43dce53ee0e3
Michael Boltachev <boltachev918@gmail.com>
2024-03-02 23:12:59 +0300

    telegram with cats

--- firstuppdate.py
+++ firstuppdate.py
@@ -1,22 +1,34 @@
 import requests
 import time
 
-API_URL = 'https://api.telegram.org/bot'
 BOT_TOKEN = '6702066413:AAGT__eOVNzp9KYehxRp6FD99gxUtRbDQvU'
-TEXT = 'Вау, новый апдейт :)'
-MAX_COUNTER:int = 100
+BOT_API = 'https://api.telegram.org/bot'
+TEXT = 'Спасибо за апдейтик'
+ERROR = 'Здесь должна была быть картинка с котиком :('
+MAX_COUNTER = 50
 
 offset = -2
-chat_id:int
+counter = 0
+cat_link: str
+cat_response: requests.Response
 
-while True:
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+while counter < MAX_COUNTER:
+    print(counter)
+    updates = requests.get(f'{BOT_API}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            name = result['message']['from']['first_name']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}Спасибо, {name}')
+            cat_response = requests.get('https://api.thecatapi.com/v1/images/search')
+            #text = result['message']['text']
+            #name = result['message']['from']['first_name']
+            if cat_response.status_code == 200:
+                #requests.get(f'{BOT_API}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}, {name} :)\nВот твое сообщение:\n{text}\nА теперь лови котика!')
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{BOT_API}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{BOT_API}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR}')
 
     time.sleep(1)
+    counter += 1
